edf49a47951ec18c63bbadf048f86753cdedf9b8
Juhize <168896778+Juhize@users.noreply.github.com>
2026-03-03 22:01:59 +0200

    Kommenteissa puutteet

--- money.py
+++ money.py
@@ -1,29 +1,41 @@
-#(tämä osuus on Willen koodia 
-# Tämä funktio arpoo saadun voidon
-# if tulos:
-lentokentta = int(input("Mille lentokentälle haluat mennä? (1-3): "))
-#     #seuraavan rivin lopussa numero "1" on game_id ja sen voi muuttaa käyttäjän muuttuvaksi game_id
-#     kentta = valitse(tulos, lentokentta, 1)
-#)
-
-#Funktioon tarvitaan rakenne, joka hakee tietokannassa pelaajan rahat, sekä lennettyjen lentojen määrän
+#Puuttuu lennettyjen lentojen määrän haku tietokannasta
 #Funktioon tarvitaan rakenne, joka mittaa etäisyyttä lentokenttien välillä, minkä mukaan lisätään menoihin lennon pituudesta riippuva elementti 
 # -Varmaan pienennän kiinteitä lentokenttämaksuja tämän jälkeen
-#Funktion loppuun tarvitaan rakenne missä uusi rahamäärä, sekä lentojen määrä tallennetaan tietokantaan
-#Pääohjelma ilmoittaa pelaajalle(-vai funktio?) uuden rahamäärän ja CO2Tax kertoimen
+#Puuttuu lentojen määrän päivitys tietokantaan
 
-#CO2Tax lisätty testin vuoksi - CO2Tax voi sitoa suoraan koodissa lennettyihin lentoihin if lauseessa
-CO2Tax = 1
 import random
-def money():
+import mysql.connector
+#Seuraavat 2 riviä on testausta varten
+#   -current_id olisi kyllä hyvä globaaliksi muuttujaksi, sehän ei muutu kesken pelin
+current_id = 1
+lentokentta = 2
+yhteys = mysql.connector.connect(
+host='127.0.0.1',
+port= 3306,
+database='flight_game',
+user='osku',
+password='1230',
+autocommit=True
+)
+
+def money(lentokentta, Flight_counter):
+    CO2Tax = 1 - (Flight_counter //3*0.2)
+    sql = f"SELECT balance FROM game WHERE id = {current_id}"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchone()
     if lentokentta == 1:
-        tuotto = random.randint(200,1000) * CO2Tax - 400
+        tuotto = tulos[0] + random.randint(200,1000) * CO2Tax - 400
     elif lentokentta == 2:
-        tuotto = random.randint(300,1500) * CO2Tax - 600
+        tuotto = tulos[0] + random.randint(300,1500) * CO2Tax - 600
     elif lentokentta == 3:
-        tuotto = random.randint(400,2000) * CO2Tax - 800
-    return int(tuotto)
+        tuotto = tulos[0] + random.randint(400,2000) * CO2Tax - 800
+    Flight_counter +=1
+    kursori.execute(f'UPDATE game SET balance = {tuotto} WHERE id={current_id}')
+    return int(tuotto), Flight_counter
+#Seuraava on testausta varten
+Flight_counter= 0
+while Flight_counter < 12:
+    tuotto, Flight_counter = money(lentokentta, Flight_counter)
 
-kierroksen_tuotto = money()
-
-print(f"Kierroksen tuotto: {kierroksen_tuotto}")
+    print(f"Kierroksen tuotto: {tuotto, Flight_counter}")
