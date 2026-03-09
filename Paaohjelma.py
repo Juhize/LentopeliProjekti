c62e961be8bc3452255e6effdeada503c95d3fd4
Willehan <wille.kuivalainen@gmail.com>
2026-03-09 13:54:03 +0200

    Paaohjelma.py

--- Paaohjelma.py
+++ Paaohjelma.py
@@ -298,8 +298,8 @@ yhteys_sql = mysql.connector.connect(
     host='127.0.0.1',
     port=3306,
     database='flight_game',
-    user='root',
-    password='AxuWaldo1?',
+    user='osku',
+    password='1230',
     autocommit=True
 )
 
